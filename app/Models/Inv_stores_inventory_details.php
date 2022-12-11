af3fdcae39d95c3af82a4d87c248d22848a5289e
Marw diab nohamed <atefdiabmohamed@gmail.com>
2022-12-11 07:21:51 +0200

    Beginning of the stores inventory process

---
+++ app/Models/Inv_stores_inventory_details.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inv_stores_inventory_details extends Model
+{
+    use HasFactory;
+    protected $table="inv_stores_inventory_details";
+    protected $fillable=[
+        'inv_stores_inventory_auto_serial', 'item_code', 'inv_uoms_id', 'batch_auto_serial', 'old_quantity', 'new_quantity', 'diffrent_quantity', 'notes', 'is_closed', 'added_by', 'created_at', 'updated_by', 'updated_at', 'cloased_by', 'closed_at', 'com_code'
+
+    ];
+}
