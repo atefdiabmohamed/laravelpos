f271b93182ced5dc2c0a7859fc555d32c0516cb5
Eng Atef Diab Mohamed <67943138+atefdiabmohamed@users.noreply.github.com>
2023-09-17 11:47:55 +0300

    ملاحظة كود الجيت هب هذا إلي الفيدو 231 بالدورة لأول 130 ساعة فقط وحجم الدورة المتوقع 300 ساعة
    ملاحظة كود الجيت هب هذا إلي الفيدو 231 بالدورة لأول 130 ساعة فقط وحجم الدورة المتوقع 300 ساعة

--- app/Http/Controllers/Admin/Inv_UomController.php
+++ app/Http/Controllers/Admin/Inv_UomController.php
@@ -17,7 +17,7 @@ public function index()
 {
 $data = Inv_uom::select()->orderby('id', 'DESC')->paginate(PAGINATION_COUNT);
 if (!empty($data)) {
-foreach ($data as $info) {
+foreach ($data as $info) { 
 $info->added_by_admin = Admin::where('id', $info->added_by)->value('name');
 if ($info->updated_by > 0 and $info->updated_by != null) {
 $info->updated_by_admin = Admin::where('id', $info->updated_by)->value('name');
