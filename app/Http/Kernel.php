316295c8666dbb4b1d788cdd9a7a597551293c6c
Eng Atef Diab Mohamed <67943138+atefdiabmohamed@users.noreply.github.com>
2024-03-23 16:38:57 +0200

    ملاحظة  كود الجيت هب الحالي

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends HttpKernel
      * The application's global HTTP middleware stack.
      *
      * These middleware are run during every request to your application.
-     *
+     * 
      * @var array<int, class-string|string>
      */
     protected $middleware = [
